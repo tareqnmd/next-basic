68dac3d9c7f940ce998a3085336f4967c35f9ae0
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-01-31 16:11:08 +0600

    link up

--- components/Navbar/Index.js
+++ components/Navbar/Index.js
@@ -5,7 +5,7 @@ export default function Index() {
 		<nav className="flex justify-between items-center w-100 shadow p-3 gap-4 link-area">
 			<Link href="/">Home</Link>
 			<>
-				<Link href="hooks">Hooks</Link>
+				<Link href="/hooks">Hooks</Link>
 			</>
 		</nav>
 	);
