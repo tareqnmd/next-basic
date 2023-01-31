bbf93d2b2447229bd0ea2fefa05f952babe8ce00
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-01-31 19:30:42 +0600

    new package added

--- components/Footer/Index.js
+++ components/Footer/Index.js
@@ -1,3 +1,11 @@
+import styles from './Footer.module.scss';
+
 export default function Index() {
-	return <div className="flex justify-center items-center w-100 border-t-2 p-3">Footer</div>;
+	return (
+		<footer
+			className={`flex justify-center items-center w-100 border-t-2 p-3 ${styles.footer}`}
+		>
+			Footer
+		</footer>
+	);
 }
--- components/Navbar/Index.js
+++ components/Navbar/Index.js
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const NavStyle = styled.header`
+	color: red;
+`;
 
 export default function Index() {
 	return (
-		<nav className="flex justify-between items-center w-100 shadow p-3 gap-4 link-area">
+		<NavStyle className="flex justify-between items-center w-100 shadow p-3 gap-4 link-area">
 			<Link href="/">Home</Link>
-			<>
+			<nav>
 				<Link href="/hooks">Hooks</Link>
-			</>
-		</nav>
+			</nav>
+		</NavStyle>
 	);
 }
