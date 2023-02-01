7bb524c731c051d3e2739a7525a699ff21d69a97
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-01 13:29:32 +0600

    btn added on

--- components/default-hooks/StateHooks.tsx
+++ components/default-hooks/StateHooks.tsx
@@ -1,10 +1,25 @@
-import Link from 'next/link';
+import { useState } from 'react';
 
 export default function StateHooks() {
+	const [type, setType] = useState('state');
 	return (
-		<>
-			<Link href="">useState</Link>
-			<Link href="">useReducer</Link>
-		</>
+		<div className="flex items-center w-100 gap-2 btn-area">
+			<button
+				className={type === 'state' ? 'btn-active' : ''}
+				onClick={() => {
+					setType('state');
+				}}
+			>
+				useState
+			</button>
+			<button
+				className={type === 'reducer' ? 'btn-active' : ''}
+				onClick={() => {
+					setType('reducer');
+				}}
+			>
+				useReducer
+			</button>
+		</div>
 	);
 }
