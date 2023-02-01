8c5b9e2267005e88dea5b203369e6241762dc166
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-01 13:05:14 +0600

    up

---
+++ pages/default-hooks/state-hooks.tsx
@@ -0,0 +1,11 @@
+import StateHooks from '@/components/default-hooks/StateHooks';
+import StateNavbar from '@/components/default-hooks/StateNavbar';
+
+export default function Index() {
+	return (
+		<>
+			<StateNavbar />
+			<StateHooks />
+		</>
+	);
+}
