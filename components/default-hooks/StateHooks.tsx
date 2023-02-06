36688b69ded583ce76d4009d0c96cd1e430b6bd6
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-06 17:40:26 +0600

    state example up

--- components/default-hooks/StateHooks.tsx
+++ components/default-hooks/StateHooks.tsx
@@ -2,6 +2,16 @@ import { useState } from 'react';
 
 export default function StateHooks() {
 	const [type, setType] = useState('state');
+	const [count, setCount] = useState(0);
+	const [step, setStep] = useState(1);
+
+	const increment = () => {
+		setCount((prev) => prev + step);
+	};
+	const decrement = () => {
+		setCount((prev) => prev - step);
+	};
+
 	return (
 		<>
 			<div className="flex items-center w-100 gap-2 btn-area">
@@ -22,6 +32,28 @@ export default function StateHooks() {
 					useReducer
 				</button>
 			</div>
+			<>
+				<div className="flex flex-col items-center justify-center">
+					<div className="count-badge my-4">{count}</div>
+					<input
+						className="input-field"
+						type="number"
+						value={step}
+						onChange={(e) => {
+							setStep(Number(e.target.value));
+						}}
+						placeholder="Step Size"
+					/>
+					<div className="flex mt-4">
+						<button className="btn-basic" onClick={increment}>
+							Increment
+						</button>
+						<button className="btn-danger" onClick={decrement}>
+							Decrement
+						</button>
+					</div>
+				</div>
+			</>
 		</>
 	);
 }
