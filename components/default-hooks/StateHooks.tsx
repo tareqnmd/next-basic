cce538f7d3f55e9899318bea974a7a264e1e67ce
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-06 17:41:48 +0600

    state up

--- components/default-hooks/StateHooks.tsx
+++ components/default-hooks/StateHooks.tsx
@@ -33,26 +33,30 @@ export default function StateHooks() {
 				</button>
 			</div>
 			<>
-				<div className="flex flex-col items-center justify-center">
-					<div className="count-badge my-4">{count}</div>
-					<input
-						className="input-field"
-						type="number"
-						value={step}
-						onChange={(e) => {
-							setStep(Number(e.target.value));
-						}}
-						placeholder="Step Size"
-					/>
-					<div className="flex mt-4">
-						<button className="btn-basic" onClick={increment}>
-							Increment
-						</button>
-						<button className="btn-danger" onClick={decrement}>
-							Decrement
-						</button>
+				{type === 'state' ? (
+					<div className="flex flex-col items-center justify-center">
+						<div className="count-badge my-4">{count}</div>
+						<input
+							className="input-field"
+							type="number"
+							value={step}
+							onChange={(e) => {
+								setStep(Number(e.target.value));
+							}}
+							placeholder="Step Size"
+						/>
+						<div className="flex mt-4">
+							<button className="btn-basic" onClick={increment}>
+								Increment
+							</button>
+							<button className="btn-danger" onClick={decrement}>
+								Decrement
+							</button>
+						</div>
 					</div>
-				</div>
+				) : (
+					<></>
+				)}
 			</>
 		</>
 	);
