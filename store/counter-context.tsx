58df2a35864a3bc1f45b57c667e612cc58a0fceb
tareqnmd <tareqnmd@gmail.com>
2023-02-08 08:31:14 +0600

    context design

--- store/counter-context.tsx
+++ store/counter-context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 type CounterValueTypes = {
 	counter: number;
@@ -8,11 +8,37 @@ type CounterValueTypes = {
 const CounterContext = createContext({
 	counter: 1,
 	step: 0,
+	increment: () => {},
+	decrement: () => {},
+	stepper: (value: number) => {},
 });
 
 export const AuthContextProvider = ({ children }: { children: any }) => {
+	const [count, setCount] = useState(0);
+	const [step, setStep] = useState(1);
+
+	const incrementHandler = () => {
+		setCount((prev) => prev + step);
+	};
+	const decrementHandler = () => {
+		setCount((prev) => prev - step);
+	};
+	const stepHandler = (value: number) => {
+		setStep(value);
+	};
+
 	return (
-		<CounterContext.Provider value={{ counter: 0, step: 0 }}>{children}</CounterContext.Provider>
+		<CounterContext.Provider
+			value={{
+				counter: count,
+				step: step,
+				increment: incrementHandler,
+				decrement: decrementHandler,
+				stepper: stepHandler,
+			}}
+		>
+			{children}
+		</CounterContext.Provider>
 	);
 };
 
