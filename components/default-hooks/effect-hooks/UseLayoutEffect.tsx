b5d72a9b640d7d890bc32bf3649682ab5c5fa9d5
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-20 18:08:50 +0600

    up

--- components/default-hooks/effect-hooks/UseLayoutEffect.tsx
+++ components/default-hooks/effect-hooks/UseLayoutEffect.tsx
@@ -1,15 +1,25 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 export default function UseLayoutEffect() {
 	const [show, setShow] = useState(false);
 	const button: { current: HTMLButtonElement | null } = useRef(null);
 	const toggle: { current: HTMLDivElement | null } = useRef(null);
+	
 	useLayoutEffect(() => {
 		if (toggle.current && button.current) {
 			const { height } = button.current?.getBoundingClientRect();
 			toggle.current.style.marginTop = height + 'px';
 		}
 	}, [show]);
+
+	// useEffect(() => {
+	// 	if (toggle.current && button.current) {
+	// 		const { height } = button.current?.getBoundingClientRect();
+	// 		toggle.current.style.marginTop = height + 'px';
+	// 	}
+	// }, [show]);
+
 	return (
 		<>
 			<button
@@ -22,8 +32,13 @@ export default function UseLayoutEffect() {
 				Toggle
 			</button>
 			{show && (
-				<div className="border-4" ref={toggle}>
-					Section Opened
+				<div className="border-4 flex items-center justify-center" ref={toggle}>
+					<Image
+						width={100}
+						height={100}
+						src="https://images.unsplash.com/photo-1676817664855-424c3ce66635"
+						alt=""
+					/>
 				</div>
 			)}
 		</>
