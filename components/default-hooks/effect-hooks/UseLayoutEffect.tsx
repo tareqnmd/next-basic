e4922b93182f90d70de332e65572b5045eed7d08
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-20 16:35:17 +0600

    layout uds

--- components/default-hooks/effect-hooks/UseLayoutEffect.tsx
+++ components/default-hooks/effect-hooks/UseLayoutEffect.tsx
@@ -1,3 +1,31 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
 export default function UseLayoutEffect() {
-	return <>UseLayoutEffect</>;
+	const [show, setShow] = useState(false);
+	const button: { current: HTMLButtonElement | null } = useRef(null);
+	const toggle: { current: HTMLDivElement | null } = useRef(null);
+	useLayoutEffect(() => {
+		if (toggle.current && button.current) {
+			const { height } = button.current?.getBoundingClientRect();
+			toggle.current.style.marginTop = height + 'px';
+		}
+	}, [show]);
+	return (
+		<>
+			<button
+				className="border-4"
+				ref={button}
+				onClick={() => {
+					setShow((prev) => !prev);
+				}}
+			>
+				Toggle
+			</button>
+			{show && (
+				<div className="border-4" ref={toggle}>
+					Section Opened
+				</div>
+			)}
+		</>
+	);
 }
