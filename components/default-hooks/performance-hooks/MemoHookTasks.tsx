901fb456f42f734e02a4d97b8ce65783340097ba
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-20 15:17:34 +0600

    memo uds

--- components/default-hooks/performance-hooks/MemoHookTasks.tsx
+++ components/default-hooks/performance-hooks/MemoHookTasks.tsx
@@ -1,42 +1,13 @@
-import { useMemo, useState } from 'react';
+import { useEffect } from 'react';
 import { TaskType } from './UseMemoHook';
 
 const MemoHookTasks = ({ tasks }: { tasks: TaskType }) => {
-	const [type, setType] = useState('active');
-
-	const filterTasks = (tasks: { name: string; type: string }[], type: string) => {
-		console.log('fnc called');
-		return type ? tasks.filter((item: { type: string }) => item.type === type) : tasks;
-	};
-
-	const reviewedTasks = useMemo(() => {
-		return filterTasks(tasks, type);
-	}, [tasks, type]);
-
-	const renderHandler = () => {
-		filterTasks(tasks, type);
-	};
-
+	useEffect(() => {
+		console.log('component rendered');
+	}, [tasks]);
 	return (
 		<>
-			<div className="btn-group mt-4">
-				<button className="bg-blue-500" onClick={() => setType('active')}>
-					Active
-				</button>
-				<button className="bg-yellow-500" onClick={() => setType('completed')}>
-					Pending
-				</button>
-				<button className="bg-red-900" onClick={() => setType('rejected')}>
-					Rejected
-				</button>
-				<button className="bg-green-900" onClick={() => setType('pending')}>
-					Completed
-				</button>
-			</div>
-			<button className="btn-basic my-4" onClick={renderHandler}>
-				Render
-			</button>
-			{reviewedTasks.map((task, index) => (
+			{tasks.map((task, index) => (
 				<div key={index}>
 					{task.name} - {task.type}
 				</div>
--- components/default-hooks/performance-hooks/UseMemoHook.tsx
+++ components/default-hooks/performance-hooks/UseMemoHook.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import MemoHookTasks from './MemoHookTasks';
 
 export type TaskType = { name: string; type: string }[];
@@ -10,11 +11,46 @@ const tasks: TaskType = [
 ];
 
 export default function UseMemoHook() {
+	const [type, setType] = useState('active');
+	const [count, setCount] = useState(0);
+
+	const filterTasks = (tasks: { name: string; type: string }[], type: string) => {
+		return type ? tasks.filter((item: { type: string }) => item.type === type) : tasks;
+	};
+
+	const reviewedTasks = useMemo(() => {
+		return filterTasks(tasks, type);
+	}, [type]);
+
+	// const reviewedTasks = filterTasks(tasks, type);
+
 	return (
 		<>
 			<div className="flex flex-col items-center justify-center">
 				<h1 className="mt-4">useMemo Example</h1>
-				<MemoHookTasks tasks={tasks} />
+				<MemoHookTasks tasks={reviewedTasks} />
+				<div className="btn-group mt-4">
+					<button className="bg-blue-500" onClick={() => setType('active')}>
+						Active
+					</button>
+					<button className="bg-yellow-500" onClick={() => setType('completed')}>
+						Pending
+					</button>
+					<button className="bg-red-900" onClick={() => setType('rejected')}>
+						Rejected
+					</button>
+					<button className="bg-green-900" onClick={() => setType('pending')}>
+						Completed
+					</button>
+				</div>
+				<button
+					className="btn-basic my-4"
+					onClick={() => {
+						setCount((prev) => prev + 1);
+					}}
+				>
+					Render {count}
+				</button>
 			</div>
 		</>
 	);
