6034a48a0b05976968a654ba4a70e7e3e77c0288
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-23 21:22:42 +0600

    use transition up

--- components/default-hooks/performance-hooks/UseTransitionHook.tsx
+++ components/default-hooks/performance-hooks/UseTransitionHook.tsx
@@ -1,3 +1,48 @@
+import { useState, useTransition } from 'react';
+const LIST_SIZE = 30000;
 export default function UseTransitionHook() {
-	return <>UseTransitionHook</>;
+	const [lists, setLists] = useState([]);
+	const [step, setStep] = useState('');
+
+	const [loading, startTransition] = useTransition();
+
+	const getEvent = (e) => {
+		const { value } = e.target;
+		setStep(value);
+
+		// const listItems = [];
+		// for (let i = 0; i < LIST_SIZE; i++) {
+		// 	listItems.push(value);
+		// }
+		// setLists(listItems);
+
+		startTransition(() => {
+			const listItems = [];
+			for (let i = 0; i < LIST_SIZE; i++) {
+				listItems.push(value);
+			}
+			setLists(listItems);
+		});
+	};
+
+	return (
+		<>
+			<input
+				className="input-field"
+				type="text"
+				value={step}
+				onChange={getEvent}
+				placeholder="Write Here"
+			/>
+			{loading ? (
+				<div>Loading....</div>
+			) : (
+				<>
+					{lists.map((list, i) => (
+						<div key={i}>{list}</div>
+					))}
+				</>
+			)}
+		</>
+	);
 }
