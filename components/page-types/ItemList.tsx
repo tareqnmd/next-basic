225420018e4c73dca87202221885b3b0a9717f0a
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-24 11:59:36 +0600

    up

--- components/page-types/ItemList.tsx
+++ components/page-types/ItemList.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 const ItemList = ({ itemData }: { itemData: [] }) => {
 	return (
-		<>
+		<div className="link-area flex items-center justify-center">
 			{itemData.map((item: { id: number; title: string }) => (
 				<Link key={item.id} href={`/page-types/ssg-ad/${item.id}`}>
 					{item.title}
 				</Link>
 			))}
-		</>
+		</div>
 	);
 };
 
