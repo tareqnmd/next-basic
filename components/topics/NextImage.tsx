fa027c6a75fe4a59955aa19eafff15f94f978b8a
Tareq <tareqatoffice@gmail.com>
2023-04-01 12:19:29 +0600

    update

--- components/topics/NextImage.tsx
+++ components/topics/NextImage.tsx
@@ -2,13 +2,17 @@ import large from '@/assets/images/large.jpg';
 import random from '@/assets/images/random.jpg';
 import Image from 'next/image';
 const NextImage = () => {
+	const loader = () => {
+		return `https://images.unsplash.com/photo-1538991383142-36c4edeaffde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80`;
+	};
 	return (
 		<>
 			<Image
-				src={large}
-				alt="Large Image"
-				placeholder="blur"
-				quality={100}
+				src="https://images.unsplash.com/photo-1680007889201-114ac772447d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+				loader={loader}
+				alt="Src Image"
+				width={500}
+				height={500}
 			/>
 			<Image
 				src={random}
@@ -16,10 +20,10 @@ const NextImage = () => {
 				// priority
 			/>
 			<Image
-				src="https://images.unsplash.com/photo-1680007889201-114ac772447d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-				alt="Src Image"
-				width={500}
-				height={500}
+				src={large}
+				alt="Large Image"
+				placeholder="blur"
+				quality={100}
 			/>
 		</>
 	);
